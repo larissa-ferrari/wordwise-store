e1f459f54135a84ade299024c7e46726f85a9d59
Larissa Ferrari <larissa.ferrari@unesp.br>
2025-06-29 00:22:45 -0300

    feat: migrations and category

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Categoria
+from .forms import CategoriaForm
 
-# Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ["id", "nome", "status"]
+    list_filter = ["status"]
+    search_fields = ["nome", "descricao"]
+    form = CategoriaForm
