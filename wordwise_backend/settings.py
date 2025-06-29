ae36e38ac93ccd8c60c80d3ef526fa45870155cf
Larissa Ferrari <larissa.ferrari@unesp.br>
2025-06-29 18:30:38 -0300

    fix: settings

--- wordwise_backend/settings.py
+++ wordwise_backend/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = "wordwise_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "wordwise",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
